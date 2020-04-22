dd3e7eb9b99315e2f75f4e9ee7b3e4bab981235b
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-04-22 13:49:29 +0200

    structure built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashboadComponent } from './pages/dashboad/dashboad.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Chart1Component } from './pages/chart1/chart1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPageFoundComponent,
+    DashboadComponent,
+    ProgressComponent,
+    Chart1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
