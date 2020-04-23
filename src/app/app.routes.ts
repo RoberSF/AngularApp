a5a414558ef0acc9f1a502aa2efd232e7787f448
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-04-23 10:47:56 +0200

    pages progress bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes, RouterModule } from "@angular/router";
-import { DashboadComponent } from './pages/dashboad/dashboad.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Chart1Component } from './pages/chart1/chart1.component';
 import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './register/register.component';
 
 
