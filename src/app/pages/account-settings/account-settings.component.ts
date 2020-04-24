1e871d8aed250717f294b1d39edf6fdab482ab66
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-04-24 17:06:46 +0200

    atribute of index changed in account-settings, how access whole DOM

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
-
-  constructor() { }
+              // esto vale para acceder a todo el DOM
+  constructor(@Inject(DOCUMENT) private _document) { }
 
   ngOnInit() {
   }
+
+changeColor(color:string) {
+  console.log(color);
+
+  // cojo la url que se hace referencia en el index.html
+  let url = `assets/css/colors/${color}.css`
+  // accedo al "id=theme" que está en el index y con set attribute le digo que a ese atributo le ponga el 2º parámetro(url)
+  this._document.getElementById('theme').setAttribute('href', url )
+}
 
 }
