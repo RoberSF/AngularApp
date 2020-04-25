c3e37eca3ca94c1c727be885632baa4082fbe830
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-04-25 13:47:08 +0200

    sacar observable del constructor, añade return, puedo coger una variable desde fuera

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -10,24 +10,9 @@ export class RxjsComponent implements OnInit {
 
   constructor() { 
 
-    let obs = new Observable(observer => {
+    
 
-      let contador = 0;
-      let interval = setInterval ( () => {
-        contador ++;
-        observer.next(contador); // va a emitir lo que le ponga
-        if ( contador ===3 ) {
-          // clearInterval(interval); // con esto se para el intervalo, pero no deja de escuchar la promesa
-          observer.complete(); // con esto le decimos que ya está todo ok
-        } 
-        if ( contador === 2) {
-          clearInterval(interval);
-          observer.error('Help')
-        }
-      }, 1000);
-    });
-
-    obs.pipe(
+    this.returnObservable().pipe(
       retry(2), // le decimos el numero de intentos que quiero que haga. Es decir,busca cada dos intentos
     ).subscribe(
       numero => {
@@ -40,5 +25,28 @@ export class RxjsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  returnObservable() { // se puede poner el tipo de observable que yo defina ":Observable<Boolean>..."
+  let obs = new Observable(observer => {
+
+    let contador = 0;
+    let interval = setInterval ( () => {
+      contador ++;
+      observer.next(contador); // va a emitir lo que le ponga
+      if ( contador ===3 ) {
+        // clearInterval(interval); // con esto se para el intervalo, pero no deja de escuchar la promesa
+        observer.complete(); // con esto le decimos que ya está todo ok
+      } 
+      if ( contador === 2) {
+        clearInterval(interval);
+        observer.error('Help')
+      }
+    }, 1000);
+  });
+return obs; // al hacer este cambio puedo pillar la variable desde fuera
+// este return lo puedo poner directamente arriba, en vez de let obs => return obs etc
+}
+
+
 
 }
