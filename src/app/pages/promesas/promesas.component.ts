3e6906ef324a0769139693e05d4fbb209f77e37f
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-04-25 12:31:48 +0200

    cambio en la ejecución de una promesa

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -12,42 +12,40 @@ export class PromesasComponent implements OnInit {
 
   // *********************************Ejemplo Promesa**********************************
   constructor() {
-
-    let promesa  = new Promise((resolve, reject) => {
-
-      let contador = 0;
-
-      let interval = setInterval(() => {
-
-        contador += 1;
-        console.log(contador);
-
-        if (contador === 3) {
-
-          resolve();
-          clearInterval(interval)
-
-        }
-
-      }, 1000);
-
-    });
-
-    promesa.then(() => console.log('terminó')).catch(error => console.log('error'))
-
-
-    }
-
-
-
-
+    this.countThree().then(() => console.log('terminó')).catch(error => console.log('error'))
+   }
 
 
   ngOnInit() {
   }
 
 
+countThree() { //podría hacer que esta promesa cuando se ejecute me devuelva un boolean, tiene una configuración especial 
 
+  let promesa  = new Promise((resolve, reject) => {
+
+    let contador = 0;
+
+    let interval = setInterval(() => {
+
+      contador += 1;
+      console.log(contador);
+
+      if (contador === 3) {
+
+        resolve();
+        clearInterval(interval) // si no pongo esto la promesa seguiría sumando 1 sin pararse
+
+      }
+
+    }, 1000);
+
+  });
+
+  
+
+  return promesa;
+}
 
 
 }
