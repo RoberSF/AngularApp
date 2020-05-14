7acd4198cc8f565dec0b85c0814249a08000c18f
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-05-14 14:12:34 +0200

    crear medico

--- src/app/pages/medicos/medico/medico.component.ts
+++ src/app/pages/medicos/medico/medico.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicosService } from '../../../services/medicos.service';
+import { NgForm } from '@angular/forms';
+import { Hospital } from '../../../models/hospital.model';
+import { HospitalService } from '../../../services/hospital.service';
+import { Medico } from 'src/app/models/medico.model';
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+
+  hospitales: Hospital[] = [];
+  medico: Medico = new Medico();
+
+  constructor(public medicoService: MedicosService, public hospitalService: HospitalService) { }
 
   ngOnInit() {
+    this.hospitalService.getHospitals().subscribe(hospitales => this.hospitales = hospitales)
   }
+
+
+guardarMedico(form: NgForm) {
+  if( form.invalid ) {
+    return
+  }
+
+  this.medicoService.saveMedico(this.medico).subscribe( medico => {
+    console.log(medico)
+  })
+}
+
+
 
 }
--- src/app/services/medicos.service.ts
+++ src/app/services/medicos.service.ts
@@ -4,13 +4,14 @@ import { URL_SERVICIOS } from '../config/config';
 import { map, filter, switchMap } from 'rxjs/operators';
 import { UsuarioService } from './usuario/usuario.service';
 import swal from 'sweetalert';
+import { Medico } from '../models/medico.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicosService {
 
-
+  url = 'http://localhost:4000'
   totalMedicos: number = 0 ;
   constructor(private http: HttpClient, public usuarioService: UsuarioService) { }
 
@@ -49,4 +50,22 @@ export class MedicosService {
                 swal('Medico Borrado');
               }))
   };
+
+
+
+  saveMedico(medico: Medico) {
+
+    var headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+       Authorization: this.usuarioService.token
+    });
+
+    var _url = this.url + '/medico';
+
+    return this.http.post(_url, medico, {headers: headers}).pipe(map( // {nombre} se pone así por que es el unico parametro que está recibiendo
+      (resp:any) => {
+        swal('Medico creado', 'success');
+        return resp.medico;
+      }));
+  }
 }
