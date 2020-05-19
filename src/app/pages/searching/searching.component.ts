f6b4d7562f6962dd556949bfd8df4c95b437194f
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-05-19 09:49:55 +0200

    componente searching works

--- src/app/pages/searching/searching.component.ts
+++ src/app/pages/searching/searching.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { Usuario } from '../../models/usuario.model';
+import { Hospital } from 'src/app/models/hospital.model';
+import { Medico } from '../../models/medico.model';
 
 @Component({
   selector: 'app-searching',
@@ -8,15 +13,37 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchingComponent implements OnInit {
 
-  constructor(public activatedRoute: ActivatedRoute) { 
+  usuarios: Usuario[] = [];
+  medicos: Medico[] = [];
+  hospitales: Hospital[] = []
+
+
+  constructor(public activatedRoute: ActivatedRoute, public http: HttpClient) { 
 
     activatedRoute.params.subscribe(params => {
       let busqueda = params['value'];
-      console.log(busqueda)
+      this.search(busqueda);
     })
   }
 
   ngOnInit() {
+  }
+
+
+  search(value: any) {
+
+    let url = URL_SERVICIOS + '/busqueda/todo/' + value;
+
+
+    this.http.get(url).subscribe(
+      (resp: any) => {
+        console.log(resp);
+        this.hospitales = resp.hospitales;
+        this.usuarios = resp.usuarios;
+        this.medicos = resp.medicos
+      }
+    )
+
   }
 
 }
